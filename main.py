e880f01e07c96f751cef5c5a983cc61c28fd012b
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2024-12-05 22:37:28 +0100

    with open path

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from pathlib import Path
 
-parent_folder_path = Path(".")
+## 1
+# parent_folder_path = Path(".")
+#
+# def parse_folder(path):
+#     for element in path.iterdir():
+#         if element.is_dir():
+#             print(f"This is folder - {element.name}")
+#             parse_folder(element)
+#         if element.is_file():
+#             print(f"This is file - {element.name}")
+#
+# parse_folder(parent_folder_path)
 
-def parse_folder(path):
-    for element in path.iterdir():
-        if element.is_dir():
-            print(f"This is folder - {element.name}")
-            parse_folder(element)
-        if element.is_file():
-            print(f"This is file - {element.name}")
+## 2
+file_name = Path("./Temp")
 
-parse_folder(parent_folder_path)
+try:
+    with open(file_name / "test.txt", "r", encoding="utf-8") as file:
+        for line in file:
+            print(line, end='')
+
+except Exception as e:
+    print(f"{e} with file")
