3e11057eaeef8fdc0a1aedeb4fb2a01605cd933d
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2024-12-05 19:33:12 +0100

    pathlib

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello world")
+from pathlib import Path
+
+parent_folder_path = Path(".")
+
+def parse_folder(path):
+    for element in path.iterdir():
+        if element.is_dir():
+            print(f"This is folder - {element.name}")
+            parse_folder(element)
+        if element.is_file():
+            print(f"This is file - {element.name}")
+
+parse_folder(parent_folder_path)
