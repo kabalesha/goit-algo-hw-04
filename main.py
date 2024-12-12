33032fd20b78add752631cd79cf106cacf09a4dd
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2024-12-12 16:41:57 +0100

    changed int into float

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def total_salary(path):
         with open(path, 'r', encoding='utf-8') as file:
             for line in file:
                 name, salary = line.strip().split(',')
-                total += int(salary)
+                total += float(salary)
                 count += 1
 
         average = total / count if count > 0 else 0
@@ -27,7 +27,7 @@ def total_salary(path):
         return 0, 0
 
 total, average = total_salary("Temp/salary.txt")
-print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+print(f"Загальна сума заробітної плати: {total:.2f}, Середня заробітна плата: {average:.2f}")
 
 # Task 2
 def get_cats_info(path):
@@ -59,3 +59,64 @@ def get_cats_info(path):
 
 cats_info = get_cats_info("Temp/cats.txt")
 print(cats_info)
+
+# Task 4
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, args
+
+def add_contact(args, contacts):
+    if len(args) != 2:
+        return "Invalid command. Usage: add [name] [phone]"
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+def change_contact(args, contacts):
+    if len(args) != 2:
+        return "Invalid command. Usage: change [name] [new phone]"
+    name, new_phone = args
+    if name in contacts:
+        contacts[name] = new_phone
+        return "Contact updated."
+    else:
+        return "Contact not found."
+
+def show_phone(args, contacts):
+    if len(args) != 1:
+        return "Invalid command. Usage: phone [name]"
+    name = args[0]
+    return contacts.get(name, "Contact not found.")
+
+def show_all(contacts):
+    if not contacts:
+        return "No contacts found."
+    return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "phone":
+            print(show_phone(args, contacts))
+        elif command == "all":
+            print(show_all(contacts))
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
